998db6c8476323f13d9c4ea67a03a62cfe2073b7
malev <marcosvanetta@gmail.com>
2015-02-18 10:50:35 -0600

    RCM extension installed

---
+++ ipython_config/nbextensions/rcm/README.md
@@ -0,0 +1,162 @@
+Revision Control Mechanism nbextension (a.k.a RCM)
+
+State of the art.
+
+The revision control mechanism extension is a simple model to make version
+control over the ipynb files (IPython notebook files). The ipynb files are
+JSON-based document persisting the data introduced by user (and the
+resulting computations) in the IPython notebook webapp client.
+There is an inherent complexity in the version control of these document
+because the nature (structure) of the ipynb file. There are a lot of metadata
+associated with each "cell" (IPython notebook units of information), and this
+metadata can change without actually changing the cell content. This is a big
+problem because usually the user wants to keep control over the content of the
+cells. Furthermore, the output of the calculations could be actually really big
+chunks of binary data which makes virtually impossible to diff the changes (and
+also result in really big files to control). These complexities make merging
+ipynb files a huge problem with a lot of conflicts which can not be solved
+easily (and sometimes it is almost impossible to solve).
+
+Our target.
+
+As you saw, version control over ipynb files is a complex problem to solve and
+a complete solution if not available yet, and probably will be not available
+soon until figured out and solve some basic problems (it would be great if we
+could say to git - or other VSC - just to apply the version control over a
+predefined structure, a part of a file, if you know something like that, let me
+know!).
+So, our solution is pointed to provide a revision control mechanism for the
+data analyst, which at first sight, they does not need to perform complex and
+advanced operations. In fact they only need a way to persist the changes in
+their ipynb files, look for previous revision points, make some diff to see the
+previous introduced changes, etc. And the RCM extension covers exactly those
+needs, but don't cover some of the previously detailed aspects/problems
+described in the State of the art section.
+
+Features.
+
+The RCM nbextension provides essentially 3 buttons to perform 3 actions:
+
+* Info: This option let you know the revision point where you are now.
+Also, it lets you see a diff between the current revision point and the latest
+changes introduced by you, but not yet commited.
+
+* Checkout: This option trigger a modal view presenting you a tree-based view
+of the previous revision points (representing the evolution of your history)
+and give you 3 option:
+
+  * Checkout a previous revision point. To do it, you have to select the desired
+  revision point a press the "OK" button. That action will load the IPython
+  notebook webapp with a "new" notebook file which is actually the "state" of
+  that notebook in the selected revision point.
+  * Make a diff comparison. If you select two previous revision points in the
+  current modal view, you can make a diff using the selected points and the
+  result will be presented as a diff view, after you press the "View diff"
+  button.
+  * Cancel, to close the current modal view.
+
+* Commit: This option lets you "commit" the current changes (I mean persist the
+changes in the current VCS supporting the extension, git in this case). In this
+way, you keep a permanent record of the changes introduced and you don't have to
+be worried about data/information lost. You have a new modal view asking for a
+commit message. After writing the message, you commit actually pressing the "OK"
+button.
+
+Implementation.
+
+The RCM is a nbextension (js-based extension) actually using the internal
+IPython machinery to perform its tasks. We actually call git command sending
+this info to the IPython kernel which, in turn, execute our requests and give
+us the resulting information that we posteriorly save it to later perform some
+other tasks in the js side.
+
+The model behind the extension and accordingly with the proposed target audience
+is a simple "linear" model on the surface but a more complex git-based branched
+model under the hood.
+Starting from a x revision point, every time the user wants to commit new
+changes, the extension perform the following actions:
+
+* stash the current work
+* create a checkout a new branch
+* unstash the previous stashed content
+* add the ipynb file
+* commit the ipynb file
+* checkout master
+* merge the branch without conflicts (-X theirs method)
+* delete the branch
+
+In this way, we get a model with "the lastest wins" strategy. Merging ipynb
+files, as described above, is really hard, so we adopt an non-conflicting
+strategy, giving the most important place to the latest changes, something that
+usually is in syncro with the data analyst workflow.
+
+With this branched implementation, we perform the checkout of previous revision
+points in a simple way just performing the following actions:
+
+* stash the current work
+* create a checkout a new branch
+(same two previous steps)
+* drop the stashed content (if you want to save it you have to commit the
+changes before visit a previous revision point).
+
+And now you are in the selected revision point. The user thinks that they went
+to a past point, but under the hood is just another branch representing the
+state of that point. The user has the option to go from here to another revision
+point (in that case the branch is deleted) or it can introduce new changes and
+commit them. In the last case, the branch follow the same path described above
+for the commit procedure, but without creating a new branch, just performs the
+next steps (from add and ahead) with same "latest win" strategy and without any
+conflicts.
+
+In this way the user can have a sort of linear "branched" model (sounds mad,
+I know) where he can try new things over previous revision points and commit the
+changes into the linear history.
+
+A note regarding the diff functionality: it is actually difficult to diff the
+ipynb content because of the reason exposed above, but to partially avoid that
+problem we perform a "flatten" of the ipynb content to get rid of some of the
+metadata information and be able to efficiently diff the actual cell content,
+keeping some additional contextual info still present, ie. the cell prompt
+number. Then we use the python diff library to actual get the diff content and
+present the result to the user in a codemirror instance.
+
+Configuration as standalone nbextension.
+
+The RCM extension can be used outside wakari as an standalone nbextension.
+To use it (and test it) you will need:
+
+* the rcm extension, living here as a PR for now, and also the utils extension:
+
+https://github.com/ContinuumIO/wakari-app-ipython/tree/feature/rcm/config/ipython_dir/nbextensions
+
+* IPython master at this specific sha commit (which is representing the IPython
+version currently deployed in Wakari Enterprise 0.5): 26753f5
+
+* Inside your .ipython folder you will find a nbextension folder, you need to
+drop the rcm and utils folders (containing the js extension and some other additional
+files) there and modify your custom.js to load the extension, ie:
+
+in .ipython/profile_default/static/custom/custom.js (or in your XXX profile)
+
+```javascript
+// we want strict javascript that fails on ambiguous syntax
+"using strict";
+
+require(["base/js/events"], function (events) {
+    events.on("status_started.Kernel", function () {
+      IPython.load_extensions('rcm/main');
+    });
+});
+```
+
+OK, that's all for now...
+
+Damian Avila
+Continuum Analytics
+
+
+
+
+
+
+
---
+++ ipython_config/nbextensions/rcm/main.js
@@ -0,0 +1,564 @@
+/**
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2014 - Continuum Analytics
+ *
+ * An extension to revision crontrol notebooks.
+ * ----------------------------------------------------------------------------
+ */
+
+define (['nbextensions/utils/main', 'components/codemirror/mode/diff/diff'], function(utils){
+
+  var execute = utils.execute;
+  var status = "merge";
+
+  /**
+   * Some utilities functions to generate the buttons
+   */
+  var generateButton = function(name, cb, options) {
+    options = options || {};
+    var r = {};
+    r[name] = {
+      "class": options.class || "btn"
+    }
+    if (cb) {
+      r[name]["click"] = cb;
+    }
+    return r
+  };
+
+  var generateOkButton = function(cb) {
+    return generateButton("OK", cb, {"class": "btn-primary"});
+  };
+
+  var cancelButton = {"Cancel": {class: "btn"}};
+
+  /**
+   * Init/reinit the repo every time you the notebook loads.
+   */
+  function initGit() {
+
+    function callback(msg) {
+      console.log("init", msg);
+    }
+
+    function error(msg){
+      console.log("init error", msg);
+    }
+
+    var command = "init = !git init";
+    var init = "init[0]";
+    execute(command, null, null);
+    execute(init, callback, error);
+
+  }
+
+  /**
+   * Get current sha
+   * @params (function) fn callback to pass the sha obtained to other functions
+   * @params (string) commit_msg the commig message you want to pass to other functions
+   */
+  function getSha(fn, commit_msg) {
+
+    function callback(msg) {
+      // TODO, make it more specific to capture only the initial commit fact
+      if (msg.substring(0,5) === "fatal") {
+          msg = "initial_commit";
+      }
+      //seven digits is enough for now...
+      smsg = msg.substring(0,7);
+      fn(smsg, commit_msg);
+      console.log("sha", smsg);
+    }
+
+    function error(msg){
+      console.log("sha error", msg);
+    }
+
+    var command = "sha = !git rev-parse HEAD";
+    var sha = "sha[0]";
+    execute(command, null, null);
+    execute(sha, callback, error);
+
+  }
+
+  /**
+   * Stash your current work and make a new branch over the sha reference point
+   * @params (function) sha the sha reference point
+   * @params (string) commit_msg the commig message you want to pass to other functions
+   */
+  function makeBranch(sha, commit_msg) {
+
+    function callback_diff(msg) {
+      console.log("diff", msg);
+//      if (msg === '' && status === "merge") {
+//          alert("You have nothing to commit yet.");
+//      }
+    }
+
+    function callback_stash(msg) {
+      // TODO, make it more specific to capture only the initial commit fact
+      if (msg.substring(0,5) === "fatal") {
+          msg = "initial_commit";
+      }
+      console.log("stash", msg);
+    }
+
+    function callback_checkout(msg) {
+      // TODO, make it more specific to capture only the initial commit fact
+//      if (msg.substring(0,5) === "fatal") {
+//          msg = "initial_commit";
+//      }
+      stasher(sha, commit_msg);
+      console.log("branch", msg);
+    }
+
+    function error(msg){
+      console.log("stash/branch error", msg);
+    }
+
+    execute("diff = !git diff --exit-code", null, null);
+    execute("diff", callback_diff, error);
+    execute("stash = !git stash", null, null);
+    execute("stash[0]", callback_stash, error);
+    execute("checkout = !git checkout " + sha + " -b ck_" + sha, null, null);
+    execute("checkout[0]", callback_checkout, error);
+
+  }
+
+  /**
+   * Unstash or drop the current stashed reference, if you unstash then make a commit
+   * @params (function) sha the sha reference point
+   * @params (string) commit_msg the commig message you want to pass to other functions
+   */
+  function stasher (sha, commit_msg) {
+
+    function callback_drop(msg) {
+      console.log("drop", msg);
+    }
+
+    function callback_unstash(msg) {
+      console.log("unstash", msg);
+      makeCommit(sha, commit_msg);
+    }
+
+    function error(msg){
+      console.log("stash error", msg);
+    }
+
+    if (status === "branch") {
+        execute("dropstash = !git stash drop", null, null);
+        execute("dropstash", callback_drop, error);
+    } else if (status === "merge"){
+        execute("unstash = !git stash pop", null, null);
+        execute("unstash", callback_unstash, error);
+    }
+
+}
+
+  /**
+   * Delete the sha-refrenced branch
+   * @params (function) sha the sha reference point
+   */
+  function deleteBranch(sha) {
+
+    function callback_delete(msg) {
+      if (msg.substring(0,31) === "error: rama «ck_initial_commit»") {
+          msg = "initial_commit";
+      }
+      console.log("delete", msg);
+    }
+
+    function error(msg){
+      // means that the lock file does not exist
+      //fn(msg);
+      console.log("error", msg);
+    }
+
+    execute("delete = !git branch -D ck_" + sha, null, null);
+    execute("delete[0]", callback_delete, error);
+
+}
+
+  /**
+   * Stage the notebook and commit the changes. Then, checkout master and
+   * merge the branch we left behind, with preference for the changes introduced
+   * by the branch (avoiding merge conflict, last changes win strategy).
+   * @params (function) sha the sha reference point
+   * @params (string) commit_msg the commig message you want to pass to other functions
+   */
+  function makeCommit(sha, commit_msg) {
+
+    function callback_add(msg) {
+      console.log("add", msg);
+    }
+
+    function callback_commit(msg) {
+      console.log("commit", msg);
+    }
+
+    function callback_master(msg) {
+      console.log("ck master", msg);
+    }
+
+    function callback_merge(msg) {
+      if (msg.substring(0,24) === "merge: ck_initial_commit") {
+          msg = "initial_commit";
+      }
+      console.log("merge", msg);
+      status = "merge"
+    }
+
+    function error(msg){
+      console.log("error", msg);
+    }
+
+    execute("add = !git add " + IPython.notebook.notebook_name, null, null);
+    execute("add", callback_add, error);
+    execute("commit = !git commit -m '" + commit_msg + "'", null, null);
+    execute("commit", callback_commit, error);
+    execute("master = !git checkout master", null, null);
+    execute("master", callback_master, error);
+    execute("merge = !git merge ck_" + sha + " -X theirs", null, null);
+    execute("merge[0]", callback_merge, error);
+
+    deleteBranch(sha);
+
+  }
+
+  /**
+   * Get the log output in a pretty and tree-view format.
+   * @params (function): callback to pass the msg file content
+   */
+  function getLog(fn) {
+
+    function callback(msg) {
+      fn(msg);
+      // console.log("log", msg);
+    }
+
+    function error(msg){
+      console.log("log error", msg);
+    }
+
+    var command = "log = !git log master --graph --pretty=format:'%h - %an, %ar : %s'"
+    // var command = "log = !git log"
+    var log = "'<br>'.join(log)";
+    execute(command, null, null);
+    execute(log, callback, error);
+
+  }
+
+  /**
+   * Execute flatten.py so python diff function is availble for getStatus.
+   * @params (string) sha1 the first hash number for a revision point
+   * @params (string) sha2 the second hash number for another revision point
+   */
+  function getStatus(sha1, sha2) {
+
+    function callback_script(msg) {
+      var retval = msg.content.data.trim();
+      console.log(retval);
+      IPython.notebook.save_notebook();
+      getDiff(shower, sha1, sha2);
+    }
+
+    function callback_dir(msg) {
+      var script = msg + "/nbextensions/rcm/flatten.py"
+      console.log("dir", script);
+      IPython.notebook.kernel.execute("%run " + script , {iopub: {output: callback_script}});
+    }
+
+    function error(msg){
+      console.log("log error", msg);
+    }
+
+    var rcm_dir = "get_ipython().ipython_dir"
+    execute(rcm_dir, callback_dir, error);
+
+  }
+
+  /**
+   * Get the git diff to see the latest changes.
+   * @params (function): callback to pass the msg file content
+   * @params (string) sha1 the first hash number for a revision point
+   * @params (string) sha2 the second hash number for another revision point
+   */
+  function getDiff(fn, sha1, sha2) {
+
+    function callback(msg) {
+      fn(msg);
+    }
+
+    function error(msg){
+      console.log("log error", msg);
+    }
+
+    var points;
+    if (sha1 === null && sha2 === null) {
+        points = "";
+    } else {
+        points = sha2 + ".." + sha1
+    }
+    var rawdiff = "_rawdiff = !git diff " + points + " --no-prefix -U1000";
+    var fladiff = "d = diff(_rawdiff)";
+    var current = "show = !git rev-parse HEAD";
+    var diff = "show.n[:7] + d";
+//    var diff = "d";
+    execute(rawdiff, null, null);
+    execute(fladiff, null, null);
+    execute(current, null, null);
+    execute(diff, callback, error);
+  }
+
+  /**
+   * Show the diff when two revision points are selected
+   * @params (string) status the git diff content to be shown
+   */
+  function shower(status) {
+    var _current = status.substring(0,7); // current sha
+    var _diff = status.substring(7,status.length); // diff text
+
+    var message = "You are now at the <b>" + _current + "</b> revision point.<br/>" +
+                  "Your latest changes are on green (or red), " +
+                  "everything else is just to give you some context.";
+
+    // TODO: Investigate why replace method is not working here...
+    if (_diff.length === 0) {
+      var cstatus = "You don't have any changes yet...";
+    } else {
+      var cstatus = _diff.split("\\'").join("\'").split('\\n').join('\n');
+    }
+
+    var textarea = $('<textarea/>')
+      .attr('rows', '13')
+      .attr('cols', '80')
+      .attr('name', 'shower')
+      .text(cstatus);
+
+    // TODO What options do we have for templating?  _.template maybe?
+    var dialogform = $('<div/>').attr('title', 'Your latest changes')
+      .append(
+        $('<form/>').append(
+          $('<fieldset/>').append(
+            $('<label/>')
+              .attr('for','shower')
+              .html(message)
+          )
+          //.append(error_div)
+          .append($('<br/>'))
+          .append(textarea)
+        )
+      );
+
+    var editor = CodeMirror.fromTextArea(textarea[0], {
+      lineNumbers: true,
+      readOnly: true,
+      mode: 'text/x-diff',
+    });
+
+    var modal = IPython.dialog.modal({
+      title: "Current Changes",
+      body: dialogform,
+      buttons: generateOkButton()
+      //notebook: IPython.notebook,
+      //keyboard_manager: IPython.keyboard_manager,
+    });
+
+    modal.on('shown.bs.modal', function(){ editor.refresh(); });
+  };
+
+
+  /**
+   * Make a checkpoint view of the sha reference points taken from git log.
+   * Then, switch to the selected reference point and reload the notebook
+   * @params (string): git log content
+   */
+  function writer(log) {
+    var empty = false;
+    var slog = log.split('<br>')
+    var long = '';
+    var oldsha;
+    var logsha;
+    var sha1, sha2;
+
+    if (log.substring(0,5) === "fatal") {
+      // TODO, make it more specific to capture only the initial commit fact
+      long = "No commits yet. Make your initial commit soon.";
+      empty = true;
+    } else {
+      for (i = 0; i < slog.length; i++) {
+        var n = slog[i].indexOf("-")
+        var ssha = slog[i].substring(n-8, n);
+        //var ssha = slog[i].substring(0,7);
+        var step;
+        if (ssha !== "") {
+          //slog[i] = slog[i].replace(" ", "&nbsp&nbsp");
+          step = '<input type=\"checkbox\" value=' + ssha + ' class=\"chk\">&nbsp' + slog[i] + '<br>';
+        } else {
+          slog[i] = slog[i].split("\\\\").join("\\");
+          step = '<span style="padding-left:21px">' + slog[i] + '<br>';
+        }
+        console.log("sub", step);
+        long = long + step;
+      }
+    }
+
+    var dialogform = $('<div/>').html('<b>' + long + '</b>').addClass('commit_message');
+
+    var alert = $('<div/>');
+    var doc = $('<div/>').addClass('alert alert-warning');
+    doc.append(
+        $('<button/>').addClass('close').attr('data-dismiss','alert').html('&times;')
+    ).append('Checkouting any revision point will <b>destroy</b> your current work.<br/>' +
+             'If you want to save it, you must <b>commit</b> your work first and then ' + 
+             'you can checkout safely.');
+    alert.append(doc).append(dialogform)
+
+    function getchkValue(){
+      var chkArray = [];
+      // look for all checkboes that have a class 'chk' attached
+      // to it and check if it was checked
+      $(".chk:checked").each(function() {
+        chkArray.push($(this).val());
+      });
+
+      if(chkArray.length === 1){
+        logsha = chkArray[0];
+        console.log(logsha);
+      } else if (chkArray.length === 2){
+        sha1 = chkArray[0];
+        sha2 = chkArray[1];
+      }
+    }
+
+    function keepOldsha(sha) {
+      oldsha = sha.substring(0,7);
+      console.log(oldsha);
+    }
+
+    getSha(keepOldsha, null);
+
+    // TODO Extract code into help functions to better document
+    var doCheckout = function() {
+      if (empty === false) {
+        getchkValue();
+        if (logsha !== undefined) {
+          IPython.notebook.save_notebook();
+          status = "branch";
+          makeBranch(logsha, null);
+          window.location.reload(true);
+          deleteBranch(oldsha);
+        } else {
+            alert("You have to select just ONE to checkout or TWO to diff.");
+        }
+      } else {
+        console.log(empty);
+      }
+    };
+
+    IPython.dialog.modal({
+      title: "CHECKOUT",
+      body: alert,
+      buttons: _.extend(
+        generateButton("View Diff", function() {
+          if (empty === false) {
+            getchkValue();
+            if (sha2 !== undefined) {
+              getStatus(sha1, sha2);
+            } else {
+              alert("You have to select just ONE to checkout or TWO to diff.");
+            }
+          } else {
+            console.log(empty);
+          }
+        }),
+        cancelButton,
+        generateOkButton(doCheckout)
+      )
+    });
+  };
+
+  /**
+   * Make an entry point for the user to write his/her commit message.
+   * Then, switch to the selected reference point and reload the notebook
+   */
+  function messager() {
+    var oldsha;
+
+    var textarea = $('<textarea/>')
+     .attr('rows','15')
+     .attr('cols','80')
+     .attr('name','commit_message')
+     .addClass('commit_message');
+
+    var dialogform = $('<div/>')
+      .text("You can write you commit message below.")
+      .append($('<br/>'))
+      .append($('<br/>'))
+      .append(textarea);
+
+    IPython.dialog.modal({
+      title: "COMMIT",
+      body: dialogform,
+      buttons: _.extend(
+        cancelButton,
+        generateOkButton(function() {
+          var str = $.trim($(textarea).val());
+          var commit_msg = str.split("\n").join(" ");
+          IPython.notebook.save_notebook();
+          if (status === "branch") {
+            getSha(makeCommit, commit_msg);
+          } else if (status === "merge") {
+            getSha(makeBranch, commit_msg);
+          }
+        })
+      ),
+      keyboard_manager: IPython.keyboard_manager
+    });
+  }
+
+  /**
+   * Inject the needed css.
+   */
+  function writerCSS() {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = require.toUrl("./nbextensions/rcm/main.css");
+    document.getElementsByTagName("head")[0].appendChild(link);
+  }
+
+  // Git init at load
+  initGit();
+
+  //css
+  writerCSS();
+
+  // UI
+  IPython.toolbar.add_buttons_group([
+    {
+      'label'   : 'status',
+      'icon'    : 'fa fa-info',
+      'callback': function() {
+        getStatus(null, null);
+      },
+      'id'      : 'gitst'
+    },
+    {
+      'label'   : 'checkout',
+      'icon'    : 'fa fa-code-fork',
+      'callback': function() {
+        getLog(writer);
+      },
+      'id'      : 'gitck'
+    },
+    {
+      'label'   : 'commit',
+      'icon'    : 'fa fa-certificate',
+      'callback': function() {
+        messager();
+      },
+      'id'      : 'gitcm'
+    },
+  ]);
+});
