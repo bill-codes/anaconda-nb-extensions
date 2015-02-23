6bc1555eecf127253cbbc56c595bf9b9e569fe9e
malev <marcosvanetta@gmail.com>
2015-02-22 18:45:34 -0600

    Move configuration detection to a python script

--- build.sh
+++ build.sh
@@ -2,21 +2,12 @@
 
 BIN=$PREFIX/bin
 EXEC=$BIN/anaconda-notebook
-IPYTHON_CONFIG=$PREFIX/ipython_config
-if [ $PY3K == 1 ]; then
-	PKG_DIR=$PKG_NAME-$PKG_VERSION-py34_$PKG_BUILDNUM/ipython_config
-else 
-	PKG_DIR=$PKG_NAME-$PKG_VERSION-py27_$PKG_BUILDNUM/ipython_config
-fi
+ETC=$PREFIX/etc/
 
 mkdir -p $BIN
-mkdir -p $IPYTHON_CONFIG
-cp -rf $RECIPE_DIR/ipython_config/* $IPYTHON_CONFIG
+mkdir -p $ETC
 
-cat <<EOF >$EXEC
-#!/bin/sh
-
-IPYTHON_DIR=$HOME/miniconda3/pkgs/$(echo $PKG_DIR) ipython notebook --profile=anaconda-notebook
-EOF
+cp -rf $RECIPE_DIR/ipython_config/ $ETC
+cp anaconda-notebook $BIN/
 
 chmod +x $EXEC
