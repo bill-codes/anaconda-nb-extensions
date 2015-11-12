feb35ba9903ff1d6df8bf33705edeee01eae11fe
damianavila <damianavila@gmail.com>
2015-11-12 19:08:46 -0300

    Modify the recipe to be compatible with the new layout and put files on consistent locations.

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# ipython_dir content into $PREFIX/config
-mkdir -p $PREFIX/config/ipython_dir
-cp -rf config/ipython_dir/* $PREFIX/config/ipython_dir
+# jupyter_config_dir content into $PREFIX/etc/anaconda_notebook/jupyter_config_dir
+mkdir -p $PREFIX/etc/anaconda_notebook/jupyter_config_dir
+cp -rf jupyter_config_dir/* $PREFIX/etc/anaconda_notebook/jupyter_config_dir
+
+# jupyter_data_dir content into $PREFIX/share/anaconda_notebook/jupyter_data_dir
+mkdir -p $PREFIX/share/anaconda_notebook/jupyter_data_dir
+cp -rf jupyter_data_dir/* $PREFIX/share/anaconda_notebook/jupyter_data_dir
 
 # notebook wrapper script into $PREFIX/bin
 mkdir -p $PREFIX/bin
