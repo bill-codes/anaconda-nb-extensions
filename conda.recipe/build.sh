896f4a2c8853210a51959c219ea524dbdb49471d
damianavila <damianavila@gmail.com>
2015-11-19 11:24:38 -0300

    Simplify the model to use the jupyter space and install the extensions at the conda level.

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # jupyter_config_dir content into $PREFIX/etc/anaconda_notebook/jupyter_config_dir
-mkdir -p $PREFIX/etc/anaconda_notebook/jupyter_config_dir
-cp -rf jupyter_config_dir/* $PREFIX/etc/anaconda_notebook/jupyter_config_dir
+mkdir -p $PREFIX/etc/jupyter
+cp -rf jupyter_config_dir/* $PREFIX/etc/jupyter
 
 # jupyter_data_dir content into $PREFIX/share/anaconda_notebook/jupyter_data_dir
-mkdir -p $PREFIX/share/anaconda_notebook/jupyter_data_dir
-cp -rf jupyter_data_dir/* $PREFIX/share/anaconda_notebook/jupyter_data_dir
+mkdir -p $PREFIX/share/jupyter
+cp -rf jupyter_data_dir/* $PREFIX/share/jupyter
 
 # notebook wrapper script into $PREFIX/bin
 mkdir -p $PREFIX/bin
