16e3f89160fb1785dd745358ed4d171dc2d6b79d
Marcos Vanetta <marcosvanetta@gmail.com>
2015-11-23 12:01:31 -0600

    Windows recipe

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,8 @@
+mkdir "%PREFIX%\etc\jupyter"
+xcopy /E jupyter_config_dir\* "%PREFIX%\etc\jupyter"
+
+mkdir "%PREFIX%\share\jupyter"
+xcopy /E jupyter_data_dir\* "%PREFIX%\share\jupyter"
+
+mkdir "%PREFIX%\bin"
+xcopy bin\* "%PREFIX%\bin"
