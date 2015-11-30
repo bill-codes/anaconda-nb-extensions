32536d3890de2b9fee6720b2938a76f37dac9d43
damianavila <damianavila@gmail.com>
2015-11-30 17:02:40 -0300

    Add install wrapeer on conda_envs/

--- jupyter_data_dir/nbserverextensions/conda_envs/conda_envs/nbextension/handlers.py
+++ jupyter_data_dir/nbserverextensions/conda_envs/conda_envs/nbextension/handlers.py
@@ -199,20 +199,11 @@ default_handlers = [
 
 def load_jupyter_server_extension(nbapp):
     """Load the nbserver extension"""
-    windows = sys.platform.startswith('win')
-    install_nbextension(static, destination='conda-envs', symlink=not windows, user=True)
     webapp = nbapp.web_app
     webapp.settings['env_manager'] = EnvManager(parent=nbapp)
 
-    cfgm = nbapp.config_manager
-    cfgm.update('tree', {
-        'load_extensions': {
-            'conda-envs/main': True,
-        }
-    })
     base_url = webapp.settings['base_url']
     webapp.add_handlers(".*$", [
         (ujoin(base_url, pat), handler)
         for pat, handler in default_handlers
     ])
-
