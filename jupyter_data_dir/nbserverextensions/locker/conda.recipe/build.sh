1b2e8dbe49008477dcc441681567bd14cfeee585
damianavila <damianavila@gmail.com>
2015-11-30 19:57:20 -0300

    Fix the recipe to install the locker conda package.

--- jupyter_data_dir/nbserverextensions/locker/conda.recipe/build.sh
+++ jupyter_data_dir/nbserverextensions/locker/conda.recipe/build.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# copy the nbserver extension into site-packages
-mkdir -p $PREFIX/lib/python$PY_VER/site-packages/locker
-cp -rf locker/* $PREFIX/lib/python$PY_VER/site-packages/locker
-
-mkdir -p $PREFIX/etc/jupyter
-cp -rf locker_config.py $PREFIX/etc/jupyter/locker_config.py
+$PYTHON setup.py install
--- jupyter_data_dir/nbserverextensions/locker/conda.recipe/post-link.sh
+++ jupyter_data_dir/nbserverextensions/locker/conda.recipe/post-link.sh
@@ -1,14 +1,1 @@
-# enable locker extension adding a line to a pre-existing config file or
-# creating a new one.
-if [ -e $PREFIX/etc/jupyter/jupyter_notebook_config.py ]; then
-    config=$(cat $PREFIX/etc/jupyter/jupyter_notebook_config.py)
-    if [[ "$config" == *"locker.nbextension"* ]]; then 
-        echo "The locker extension is already detected/enabled in your jupyter_notebook_config.py file."
-    else
-        echo "The locker extension was enabled in your jupyter_notebook_config.py file."
-        echo "c.NotebookApp.server_extensions.append('locker.nbextension')" >> $PREFIX/etc/jupyter/jupyter_notebook_config.py
-    fi
-else
-    echo "We did not detect a jupyter_notebook_config.py file, we created a new one and enable the locker extension."
-    cp -f $PREFIX/etc/jupyter/locker_config.py $PREFIX/etc/jupyter/jupyter_notebook_config.py
-fi
+"${PREFIX}/bin/python" -m locker.install --enable --prefix="${PREFIX}"
