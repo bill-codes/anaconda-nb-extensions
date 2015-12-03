6524c3b7811492d340bd7f9bc864cd5505596236
Marcos Vanetta <marcosvanetta@gmail.com>
2015-12-03 01:35:19 -0600

    Only server side extension

---
+++ examples/nbserverextension/hello_world/nbextension/handlers.py
@@ -0,0 +1,19 @@
+from notebook.utils import url_path_join
+from notebook.base.handlers import IPythonHandler
+
+
+class HelloWorldHandler(IPythonHandler):
+    def get(self):
+        self.finish('Hello, world!')
+
+
+def load_jupyter_server_extension(nb_app):
+    '''
+    Register a hello world handler.
+
+    Based on https://github.com/Carreau/jupyter-book/blob/master/extensions/server_ext.py
+    '''
+    web_app = nb_app.web_app
+    host_pattern = '.*$'
+    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
+    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
