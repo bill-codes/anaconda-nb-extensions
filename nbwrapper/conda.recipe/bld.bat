7a4cc8d4d4cbbd9f75b4d8dd0ad147dff2748ea6
Marcos Vanetta <marcosvanetta@gmail.com>
2015-12-07 14:58:50 -0600

    nbwrapper .binstar.yml file

--- nbwrapper/conda.recipe/bld.bat
+++ nbwrapper/conda.recipe/bld.bat
@@ -1,11 +1,2 @@
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
-
-mkdir                            "%PREFIX%\etc\jupyter"
-xcopy /E jupyter_config_dir\*    "%PREFIX%\etc\jupyter"
-
-mkdir                                       "%PREFIX%\share\jupyter\nbextensions"
-xcopy /E jupyter_data_dir\nbextensions\*    "%PREFIX%\share\jupyter\nbextensions"
-
-mkdir          "%PREFIX%\Scripts"
-xcopy bin\*    "%PREFIX%\Scripts"
